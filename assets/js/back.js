8c3dc0cd7b13c52b3ee0ba205a2a5d855a872fa6
mongmolong <143096636+mongmolong@users.noreply.github.com>
2024-10-21 23:00:27 +0900

    third

--- assets/js/back.js
+++ assets/js/back.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.1 
+      threshold: 0.5
   };
 
   const observer = new IntersectionObserver((entries) => {
